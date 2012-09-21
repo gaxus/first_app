8ab989cfcf213bd362b042f4dcabd80adbe04b43
gaxus <bouvier.charlelie@gmail.com>
2012-09-21 11:55:36 +0200

    Import initial.

---
+++ Documents/2012-2013/IFPS/rails_project/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4424f18eca1562f8d18ac9a71f16c1808a1eff8dd7c6fa2ce1f121317fc43b47f554980c01f761012433499f024892d1cb3f7ceff34e786103476b550d848812'
